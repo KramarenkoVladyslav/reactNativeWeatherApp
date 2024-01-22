7a0e3aa18c0b883b0d638a7c5d8fef18ca4f56b3
Vladyslav <kramarenko.creator@gmail.com>
2024-01-22 15:02:09 +0200

    fix: API weather.js

--- api/weather.js
+++ api/weather.js
@@ -1,30 +1,31 @@
 import axios from 'axios';
-
 import { apiKey } from '../constants';
 
 const forecastEndpoint = params =>
-	`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&days=${params.days}1&aqi=no&alerts=no`;
+	`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&days=${params.days}`;
 const locationsEndpoint = params =>
 	`https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${params.cityName}`;
-
-const apiCall = async (endpoint) => {
+const apiCall = async endpoint => {
 	const options = {
 		method: 'GET',
-		url: endpoint
-	} 
+		url: endpoint,
+	};
+
 	try {
 		const response = await axios.request(options);
 		return response.data;
-	} catch (err) {
-		console.log('error', err)
-		return null
+	} catch (error) {
+		console.log('error: ', error);
+		return {};
 	}
-}
+};
 
 export const fetchWeatherForecast = params => {
-	return apiCall(forecastEndpoint(params));
-}
+	let forecastUrl = forecastEndpoint(params);
+	return apiCall(forecastUrl);
+};
 
 export const fetchLocations = params => {
-	return apiCall(locationsEndpoint(params))
-}
+	let locationsUrl = locationsEndpoint(params);
+	return apiCall(locationsUrl);
+};
